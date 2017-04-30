006136ed5667729264bf209236973352fa1cfb6b
Anand Bait <anand.bait@gmail.com>
2017-04-30 15:53:15 +0530

    Update index views as per new template

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
+  resources :payment_sources
+  resources :accounts
   resources :transfers
   resources :transaction_purposes
   resources :transactions
   resources :sub_categories
   resources :categories
   resources :account_balances
-  resources :accounts
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

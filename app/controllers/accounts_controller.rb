c44a5b59223b9a09589aa2d6631bf462b0641713
Anand Bait <anand.bait@gmail.com>
2017-05-02 07:31:30 +0530

    Update Account scaffold as per modified templates

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,74 +1,51 @@
 class AccountsController < ApplicationController
   before_action :set_account, only: [:show, :edit, :update, :destroy]
 
-  # GET /accounts
-  # GET /accounts.json
   def index
     @accounts = Account.all
   end
 
-  # GET /accounts/1
-  # GET /accounts/1.json
-  def show
-  end
-
-  # GET /accounts/new
   def new
     @account = Account.new
+    render layout: false
   end
 
-  # GET /accounts/1/edit
-  def edit
-  end
-
-  # POST /accounts
-  # POST /accounts.json
   def create
     @account = Account.new(account_params)
-
-    respond_to do |format|
-      if @account.save
-        format.html { redirect_to @account, notice: 'Account was successfully created.' }
-        format.json { render :show, status: :created, location: @account }
-      else
-        format.html { render :new }
-        format.json { render json: @account.errors, status: :unprocessable_entity }
-      end
+    if @account.save
+      redirect_to accounts_url, notice: 'Account was successfully created.'
+    else
+      render :new
     end
   end
 
-  # PATCH/PUT /accounts/1
-  # PATCH/PUT /accounts/1.json
+  def edit
+    render layout: false
+  end
+
   def update
-    respond_to do |format|
-      if @account.update(account_params)
-        format.html { redirect_to @account, notice: 'Account was successfully updated.' }
-        format.json { render :show, status: :ok, location: @account }
-      else
-        format.html { render :edit }
-        format.json { render json: @account.errors, status: :unprocessable_entity }
-      end
+    if @account.update(account_params)
+      redirect_to accounts_url, notice: 'Account was successfully updated.'
+    else
+      render :edit
     end
   end
 
-  # DELETE /accounts/1
-  # DELETE /accounts/1.json
+  def show
+    render layout: false
+  end
+
   def destroy
     @account.destroy
-    respond_to do |format|
-      format.html { redirect_to accounts_url, notice: 'Account was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    redirect_to accounts_url, notice: 'Account was successfully destroyed.'
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_account
       @account = Account.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
     def account_params
-      params.fetch(:account, {})
+      params.require(:account).permit(:name, :description, :details, :account_type)
     end
 end

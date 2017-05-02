87f6e9790e14b98273948aa62e26e87a57205950
Anand Bait <anand.bait@gmail.com>
2017-05-02 07:32:48 +0530

    Update AccountBalance scaffold as per modified templates

--- app/controllers/account_balances_controller.rb
+++ app/controllers/account_balances_controller.rb
@@ -1,74 +1,51 @@
 class AccountBalancesController < ApplicationController
   before_action :set_account_balance, only: [:show, :edit, :update, :destroy]
 
-  # GET /account_balances
-  # GET /account_balances.json
   def index
     @account_balances = AccountBalance.all
   end
 
-  # GET /account_balances/1
-  # GET /account_balances/1.json
-  def show
-  end
-
-  # GET /account_balances/new
   def new
     @account_balance = AccountBalance.new
+    render layout: false
   end
 
-  # GET /account_balances/1/edit
-  def edit
-  end
-
-  # POST /account_balances
-  # POST /account_balances.json
   def create
     @account_balance = AccountBalance.new(account_balance_params)
-
-    respond_to do |format|
-      if @account_balance.save
-        format.html { redirect_to @account_balance, notice: 'Account balance was successfully created.' }
-        format.json { render :show, status: :created, location: @account_balance }
-      else
-        format.html { render :new }
-        format.json { render json: @account_balance.errors, status: :unprocessable_entity }
-      end
+    if @account_balance.save
+      redirect_to account_balances_url, notice: 'Account balance was successfully created.'
+    else
+      render :new
     end
   end
 
-  # PATCH/PUT /account_balances/1
-  # PATCH/PUT /account_balances/1.json
+  def edit
+    render layout: false
+  end
+
   def update
-    respond_to do |format|
-      if @account_balance.update(account_balance_params)
-        format.html { redirect_to @account_balance, notice: 'Account balance was successfully updated.' }
-        format.json { render :show, status: :ok, location: @account_balance }
-      else
-        format.html { render :edit }
-        format.json { render json: @account_balance.errors, status: :unprocessable_entity }
-      end
+    if @account_balance.update(account_balance_params)
+      redirect_to account_balances_url, notice: 'Account balance was successfully updated.'
+    else
+      render :edit
     end
   end
 
-  # DELETE /account_balances/1
-  # DELETE /account_balances/1.json
+  def show
+    render layout: false
+  end
+
   def destroy
     @account_balance.destroy
-    respond_to do |format|
-      format.html { redirect_to account_balances_url, notice: 'Account balance was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    redirect_to account_balances_url, notice: 'Account balance was successfully destroyed.'
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_account_balance
       @account_balance = AccountBalance.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
     def account_balance_params
-      params.fetch(:account_balance, {})
+      params.require(:account_balance).permit(:opening_balance, :calculated_closing_balance, :actual_closing_balance, :month, :year, :account_id)
     end
 end

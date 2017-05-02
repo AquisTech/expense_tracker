6f7bd0ba32fd7bfef558282899c3885f51f1a692
Anand Bait <anand.bait@gmail.com>
2017-05-02 07:41:17 +0530

    Update TransactionPurpose scaffold as per modified templates

--- app/controllers/transaction_purposes_controller.rb
+++ app/controllers/transaction_purposes_controller.rb
@@ -1,74 +1,51 @@
 class TransactionPurposesController < ApplicationController
   before_action :set_transaction_purpose, only: [:show, :edit, :update, :destroy]
 
-  # GET /transaction_purposes
-  # GET /transaction_purposes.json
   def index
     @transaction_purposes = TransactionPurpose.all
   end
 
-  # GET /transaction_purposes/1
-  # GET /transaction_purposes/1.json
-  def show
-  end
-
-  # GET /transaction_purposes/new
   def new
     @transaction_purpose = TransactionPurpose.new
+    render layout: false
   end
 
-  # GET /transaction_purposes/1/edit
-  def edit
-  end
-
-  # POST /transaction_purposes
-  # POST /transaction_purposes.json
   def create
     @transaction_purpose = TransactionPurpose.new(transaction_purpose_params)
-
-    respond_to do |format|
-      if @transaction_purpose.save
-        format.html { redirect_to @transaction_purpose, notice: 'Transaction purpose was successfully created.' }
-        format.json { render :show, status: :created, location: @transaction_purpose }
-      else
-        format.html { render :new }
-        format.json { render json: @transaction_purpose.errors, status: :unprocessable_entity }
-      end
+    if @transaction_purpose.save
+      redirect_to transaction_purposes_url, notice: 'Transaction purpose was successfully created.'
+    else
+      render :new
     end
   end
 
-  # PATCH/PUT /transaction_purposes/1
-  # PATCH/PUT /transaction_purposes/1.json
+  def edit
+    render layout: false
+  end
+
   def update
-    respond_to do |format|
-      if @transaction_purpose.update(transaction_purpose_params)
-        format.html { redirect_to @transaction_purpose, notice: 'Transaction purpose was successfully updated.' }
-        format.json { render :show, status: :ok, location: @transaction_purpose }
-      else
-        format.html { render :edit }
-        format.json { render json: @transaction_purpose.errors, status: :unprocessable_entity }
-      end
+    if @transaction_purpose.update(transaction_purpose_params)
+      redirect_to transaction_purposes_url, notice: 'Transaction purpose was successfully updated.'
+    else
+      render :edit
     end
   end
 
-  # DELETE /transaction_purposes/1
-  # DELETE /transaction_purposes/1.json
+  def show
+    render layout: false
+  end
+
   def destroy
     @transaction_purpose.destroy
-    respond_to do |format|
-      format.html { redirect_to transaction_purposes_url, notice: 'Transaction purpose was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    redirect_to transaction_purposes_url, notice: 'Transaction purpose was successfully destroyed.'
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_transaction_purpose
       @transaction_purpose = TransactionPurpose.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
     def transaction_purpose_params
-      params.fetch(:transaction_purpose, {})
+      params.require(:transaction_purpose).permit(:name, :sub_category_id)
     end
 end

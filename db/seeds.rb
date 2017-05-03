1fa85ca0dfb764cd9d088350c08783644b84558d
Anand Bait <anand.bait@gmail.com>
2017-05-03 09:25:09 +0530

    Add Default Seeding data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,31 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+[
+  {name: 'SBI Saving Bank Account', account_type: 'SB'},
+  {name: 'HDFC Saving Bank Account', account_type: 'SB'},
+  {name: 'ICICI Saving Bank Account', account_type: 'SB'},
+  {name: 'StanC Saving Bank Account', account_type: 'SB'},
+  {name: 'HDFC Credit Card Account', account_type: 'CC'},
+  {name: 'PayTM', account_type: 'EW'},
+  {name: 'Mobikwik', account_type: 'EW'},
+  {name: 'OlaMoney', account_type: 'EW'},
+  {name: 'R Wallet', account_type: 'EW'},
+  {name: 'ATVM Card', account_type: 'SC'},
+  {name: 'ICICI Food Card', account_type: 'SC'},
+  {name: 'Cash', account_type: 'CS'}
+].each do |acc|
+  Account.where(
+    name: acc[:name], account_type: acc[:account_type],
+    description: acc[:description] || acc[:name],
+    details: acc[:details] || acc[:name]
+  ).first_or_create
+end
+['Food', 'Travel'].each do |cat|
+  Category.where(name: cat).first_or_create
+end
+[
+
+].each do |sub_cat|
+  SubCategory.create()
+end

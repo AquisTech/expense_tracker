e040551eae8bbba6a5c107829c8d442602f0800d
Anand Bait <anand.bait@gmail.com>
2017-05-08 06:41:40 +0530

    Add generator config that is used

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,10 @@ module ExpenseTracker
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.generators do |g|
+      g.orm false
+      g.assets false
+      g.test_framework  false
+    end
   end
 end

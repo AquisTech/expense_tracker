f84e9be350b86137d6d1a2b8a63606fdaef733f9
AquisTech <anand.bait@gmail.com>
2018-02-23 03:03:28 +0530

    Changes in HomeController to get custom add at top

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,11 +2,11 @@ class HomeController < ApplicationController
   def index
     # transaction_purposes = TransactionPurpose.for(Date.today || params[:date])
     transaction_purposes = TransactionPurpose.first(5)
-    @transactions = []
+    @transactions = [Transaction.new]
+    # TODO: Keep new on top and filled ones below
     transaction_purposes.each do |tp|
       @transactions << (tp.transactions.present? ? tp.transactions : tp.transactions.build) # add scope in tp.transactions.for(Date.today || params[:date])
     end
-    @transactions << Transaction.new
     puts '=====================', @transactions.inspect
   end
 end

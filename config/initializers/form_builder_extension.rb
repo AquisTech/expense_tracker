2edbb93ef78c9da6afc3ebaa306a13d3d7709fbb
AquisTech <anand.bait@gmail.com>
2018-03-24 16:44:14 +0530

    Changes for value of persisted object

--- config/initializers/form_builder_extension.rb
+++ config/initializers/form_builder_extension.rb
@@ -7,7 +7,7 @@ class ActionView::Helpers::FormBuilder
     classes.concat(classes.is_a?(Array) ? ['input-group-field'] : ' input-group-field')
     options[:class] = classes
     options[:min] = options[:min] || 0
-    options[:value] = options[:value] || 0 # TODO: Check for value of persisted object
+    options[:value] = self.object.try(:send, method) || options[:value] || 0
     content_tag :div, class: 'input-group' do
       content_tag(:span, options[:currency].html_safe || '$', class: 'input-group-label') +
       number_field(method, options)

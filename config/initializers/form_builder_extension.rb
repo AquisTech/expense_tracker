c01607bf8fd54ad344184c9636cbef3ab11244a9
AquisTech <anand.bait@gmail.com>
2018-03-24 16:05:27 +0530

    Modify amount_field form_builder to include defaults min and value

--- config/initializers/form_builder_extension.rb
+++ config/initializers/form_builder_extension.rb
@@ -6,6 +6,8 @@ class ActionView::Helpers::FormBuilder
     classes = options[:class] || ''
     classes.concat(classes.is_a?(Array) ? ['input-group-field'] : ' input-group-field')
     options[:class] = classes
+    options[:min] = options[:min] || 0
+    options[:value] = options[:value] || 0 # TODO: Check for value of persisted object
     content_tag :div, class: 'input-group' do
       content_tag(:span, options[:currency].html_safe || '$', class: 'input-group-label') +
       number_field(method, options)

b5466a4db59ef5f4e57c9079ac841300ea81b515
AquisTech <anand.bait@gmail.com>
2018-03-24 18:49:48 +0530

    Add TODO for currecy customization

--- config/initializers/form_builder_extension.rb
+++ config/initializers/form_builder_extension.rb
@@ -9,7 +9,7 @@ class ActionView::Helpers::FormBuilder
     options[:min] = options[:min] || 0
     options[:value] = self.object.try(:send, method) || options[:value] || 0
     content_tag :div, class: 'input-group' do
-      content_tag(:span, options[:currency].html_safe || '$', class: 'input-group-label') +
+      content_tag(:span, options[:currency].html_safe || '$', class: 'input-group-label') + # TODO: Currency as per user preference
       number_field(method, options)
     end.html_safe
   end

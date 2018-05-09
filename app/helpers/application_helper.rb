71265a38c6e7b121f02c41ccc41bf924bf2afe48
AquisTech <anand.bait@gmail.com>
2018-05-10 01:54:14 +0530

    Flash message helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,24 @@ module ApplicationHelper
   def hide_if(condition)
     :hide if condition
   end
+
+  def zf_class_for flash_type
+    { success: 'success', error: 'alert', failure: 'alert', alert: 'warning', notice: 'primary' }[flash_type.to_sym]
+  end
+
+  def flash_messages
+    flash.map do |msg_type, message|
+      content_tag(:div, class: "callout #{zf_class_for(msg_type)}", data: { closable: true }) do
+        (button_tag(type: 'button', class: 'close-button', data: { close: true }) do
+          content_tag(:span, '&times;'.html_safe)
+        end + message).html_safe
+      end
+    end.join.html_safe
+  end
+
+  def flash_messages_with_grid_container
+    content_tag(:div, class: 'grid-container') do
+      flash_messages
+    end
+  end
 end

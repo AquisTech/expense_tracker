7b99fe4ecf209066be79f14be43d5edf9ea34eb0
AquisTech <anand.bait@gmail.com>
2018-05-10 01:56:22 +0530

    Show success and failure on create and update of transaction

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -15,9 +15,13 @@ class TransactionsController < ApplicationController
   def create
     @transaction = Transaction.new(transaction_params)
     if @transaction.save
-      render :success, locals: {tp_id: @transaction.transaction_purpose_id}
+      new_transaction = Transaction.new
+      new_transaction.payments.build
+      flash[:success] = 'Transaction created successfully.'
+      render :success, locals: { transaction: @transaction, new_transaction: new_transaction }
     else
-      render :failure, locals: {transaction: @transaction}
+      flash[:failure] = @transaction.errors.full_messages.to_sentence
+      render :failure, locals: { transaction: @transaction }
     end
   end
 
@@ -29,9 +33,11 @@ class TransactionsController < ApplicationController
   # This action is called from home/index
   def update
     if @transaction.update(transaction_params)
-      render :success, locals: {tp_id: @transaction.transaction_purpose_id}
+      flash[:success] = 'Transaction updated successfully.'
+      render :success, locals: { transaction: @transaction }
     else
-      render :failure, locals: {transaction: @transaction}
+      flash[:failure] = @transaction.errors.full_messages.to_sentence
+      render :failure, locals: { transaction: @transaction }
     end
   end
 
@@ -40,8 +46,11 @@ class TransactionsController < ApplicationController
   end
 
   def destroy
-    @transaction.destroy
-    redirect_to transactions_url, notice: 'Transaction was successfully destroyed.'
+    if @transaction.destroy
+      redirect_to transactions_url, success: 'Transaction destroyed successfully.'
+    else
+      redirect_to transactions_url, failure: 'Transaction failed to destroy.'
+    end
   end
 
   private

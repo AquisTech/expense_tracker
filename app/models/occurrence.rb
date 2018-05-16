cc6c6f0babd9b13e1163dfa29241f7b0080d5adf
AquisTech <anand.bait@gmail.com>
2018-05-16 22:04:52 +0530

    Occurrence: starts_on validation for timeliness

--- app/models/occurrence.rb
+++ app/models/occurrence.rb
@@ -7,6 +7,6 @@ class Occurrence < ApplicationRecord
   validates :weeks, numericality: { only_integer: true, greater_than_or_equal_to: -1 }, allow_nil: true
   validates :months, numericality: { only_integer: true, greater_than: 0 }, allow_nil: true
   validates :count, numericality: { only_integer: true, greater_than: 0 }, allow_nil: true
-  validates :starts_on, presence: true
+  validates :starts_on, presence: true, timeliness: true
   validates :ends_on, timeliness: { after: :starts_on }, allow_nil: true
 end

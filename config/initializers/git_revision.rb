5d1a4dbb6947414221992784b6f9daaf0fe2e832
AquisTech <anand.bait@gmail.com>
2019-01-12 00:09:41 +0530

    Display latest git version in footer- changes for heroku

--- config/initializers/git_revision.rb
+++ config/initializers/git_revision.rb
@@ -1,5 +1,14 @@
 # Get the deployed git revision to display in the footer
 module Git
-  REVISION = `SHA1=$(git rev-parse --short HEAD 2> /dev/null); if [ $SHA1 ]; then echo $SHA1; else echo 'unknown'; fi`.chomp
-  VERSION = `VERSION=$(git describe --tags 2> /dev/null); if [ $VERSION ]; then echo $VERSION; else echo 'unknown'; fi`.chomp
+  REVISION = if Rails.env.production?
+    revision = `SHA1=$(heroku releases -n=1 --json); if [ $SHA1 ]; then echo $SHA1; else echo 'unknown'; fi`.chomp
+    "#{revision['description'].gsub(/Deploy/)}"
+  else
+    `SHA1=$(git rev-parse --short HEAD 2> /dev/null); if [ $SHA1 ]; then echo $SHA1; else echo 'unknown'; fi`.chomp
+  end
+  VERSION = if Rails.env.production?
+    "V#{revision['version']}"
+  else
+    `VERSION=$(git describe --tags 2> /dev/null); if [ $VERSION ]; then echo $VERSION; else echo 'unknown'; fi`.chomp
+  end
 end

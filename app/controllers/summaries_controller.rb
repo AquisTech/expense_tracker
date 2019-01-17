486f346f9c16c75b8967b511802603411bb8bb2f
AquisTech <anand.bait@gmail.com>
2019-01-18 00:01:48 +0530

    Monthly and yearly summary

--- app/controllers/summaries_controller.rb
+++ app/controllers/summaries_controller.rb
@@ -1,6 +1,14 @@
 class SummariesController < ApplicationController
   def show
-    @date = Date.parse(params[:date]) rescue Date.today
+    @year = Date.parse("JAN-#{params[:year]}") if params[:year] rescue raise 'Invalid year'
+    @month = Date.parse(params[:month]) if params[:month] rescue raise 'Invalid month'
+    if @year
+      @date = @year.beginning_of_year..@year.end_of_year
+    elsif @month
+      @date = @month.beginning_of_month..@month.end_of_month
+    else
+      @date = Date.parse(params[:date]) rescue Date.today
+    end
     @transactions = Transaction.where(transacted_at: @date)
   end
 end

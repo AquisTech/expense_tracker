c8d56936d94ab3ad2208affccb90bdcce5891280
AquisTech <anand.bait@gmail.com>
2019-02-02 14:24:52 +0530

    Google outh integration - correction

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -258,7 +258,7 @@ Devise.setup do |config|
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
   config.omniauth :google_oauth2,
-    ENV['GOOGLE_CLIENT_ID'], ['GOOGLE_CLIENT_SECRET'],
+    ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET'],
     {
       scope: "userinfo.email,userinfo.profile,contacts.readonly,user.phonenumbers.read,user.birthday.read,plus.login"
     }

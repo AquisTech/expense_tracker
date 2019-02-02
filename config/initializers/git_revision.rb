658ec0d6c03f5bb43bf7bb30598e5f337e3a317d
AquisTech <anand.bait@gmail.com>
2019-02-02 19:18:18 +0530

    Changes for git info

--- config/initializers/git_revision.rb
+++ config/initializers/git_revision.rb
@@ -2,4 +2,5 @@
 module Git
   REVISION = `SHA1=$(git rev-parse --short HEAD 2> /dev/null); if [ $SHA1 ]; then echo $SHA1; else echo 'unknown'; fi`.chomp
   VERSION = `VERSION=$(git describe --tags 2> /dev/null); if [ $VERSION ]; then echo $VERSION; else echo 'unknown'; fi`.chomp
+  BRANCH = `BRANCH=$(git rev-parse --abbrev-ref HEAD 2> /dev/null); if [ $BRANCH ]; then echo $BRANCH; else echo 'unknown'; fi`.chomp
 end

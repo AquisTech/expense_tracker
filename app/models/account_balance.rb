304c82be9634d8f32621fcb8eced24d729dc621e
AquisTech <anand.bait@gmail.com>
2019-02-09 19:36:35 +0530

    Make nested account balance form compact

--- app/models/account_balance.rb
+++ app/models/account_balance.rb
@@ -7,8 +7,10 @@ class AccountBalance < ApplicationRecord
   validates :opening_balance, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   validates :calculated_closing_balance, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   validates :actual_closing_balance, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
   before_validation {
     self.user_id = self.account.user_id
+    self.actual_closing_balance = 0 unless self.actual_closing_balance
   }
   before_create {
     self.calculated_closing_balance = self.opening_balance

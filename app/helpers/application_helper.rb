fc16d64c57875b9137c8355c6230cfe14f802a76
AquisTech <anand.bait@gmail.com>
2019-02-26 13:46:47 +0530

    Invite via whatsapp

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -46,4 +46,13 @@ module ApplicationHelper
     link_to icon_tag('delete'), object, method: :delete, data: { confirm: "Are you sure you want to delete this #{object.class.name}?" }, class: 'button clear alert'
   end
 
+  def invite_for_registration_via_whatsapp
+    url = build_whatsapp_url("Hey Buddy! Join me on MoneyBee, an expense tracking, budgeting and finance planning app. #{new_user_registration_url}")
+    link_to 'Invite via WhatsApp', url, class: 'button success hollow'
+  end
+
+  def build_whatsapp_url(message, recipient = nil)
+    "https://wa.me/#{(recipient + '/') if recipient}?text=#{URI.encode(message)}"
+  end
+
 end

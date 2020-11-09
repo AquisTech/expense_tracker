5b54471b4068d1b8fc6622f0ee9a3c8493a59c33
AquisTech <anand.bait@gmail.com>
2020-11-10 02:02:53 +0530

    Seed from factory method to provide easy seeding of data in non prod env

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,11 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+
+  unless Rails.env.prod?
+    def self.seed_from_factory(count = 1)
+      count.times do
+        FactoryBot.create(self.to_s.underscore.to_sym)
+      end
+    end
+  end
 end

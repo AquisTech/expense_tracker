e77914a4a4457abd0ab11a878b39f5e8626365e6
AquisTech <anand.bait@gmail.com>
2020-11-15 12:48:34 +0530

    Update model scaffold association bugs

--- lib/templates/active_record/model/model.rb
+++ lib/templates/active_record/model/model.rb
@@ -21,10 +21,10 @@ class <%= class_name %> < <%= parent_class_name.classify %>
   # Callbacks
 
   # Associations
-  <%- belongs_to_attrs = attributes_names.select{ |attr, value| k.ends_with?('_id') } -%>
-  <% belongs_to_attrs.each do |attr, value| -%>
-belongs_to :<%= attr %><%= ', polymorphic: true' if attribute_names.keys.include?(attr.gsub(/_id$/, '_type')) %>
-  <% end -%>
+<%- belongs_to_attrs = attributes_names.select{ |attr, value| attr.ends_with?('_id') } -%>
+<% belongs_to_attrs.each do |attr, value| -%>
+  belongs_to :<%= attr.gsub(/_id$/, '') %><%= ', polymorphic: true' if attributes_names.keys.include?(attr.gsub(/_id$/, '_type')) %>
+<% end -%>
 <% attributes.select(&:reference?).each do |attribute| -%>
   belongs_to :<%= attribute.name %><%= ', polymorphic: true' if attribute.polymorphic? %><%= ', required: true' if attribute.required? %>
 <% end -%>

abe7edb11b0a283d6e89becf78694112cc843b00
AquisTech <anand.bait@gmail.com>
2020-11-15 15:31:40 +0530

    Update model scaffold for handling polymorphic _type attribute

--- lib/templates/active_record/model/model.rb
+++ lib/templates/active_record/model/model.rb
@@ -1,9 +1,14 @@
 <% module_namespacing do -%>
 class <%= class_name %> < <%= parent_class_name.classify %>
   # Validations
-  <%- attributes_names = attributes.empty? ? singular_table_name.classify.constantize.columns_hash.except('id', 'created_at', 'updated_at') : attributes -%>
-<% attributes_names.each do |attr, value| -%>
-<% next if attr.ends_with?('_id') -%>
+<%- attributes_names = singular_table_name.classify.constantize.columns_hash.except('id', 'created_at', 'updated_at') -%>
+<% assoc_attrs = attributes_names.select { |attr, value| attr.ends_with?('_id') } -%>
+<% assoc_attrs.dup.each do |attr, value| -%>
+<% type_attr = attr.gsub(/_id$/, '_type') -%>
+<% assoc_attrs[type_attr] = attributes_names[type_attr] if attributes_names.keys.include?(type_attr) -%>
+<% end -%>
+<% non_assoc_attrs = attributes_names.except(*assoc_attrs.keys) -%>
+<% non_assoc_attrs.each do |attr, value| -%>
   validates :<%= attr %>, presence: true<%- if value.type == :integer -%>
 , numericality: { only_integer: true }
 <%- elsif value.type == :float -%>
@@ -21,9 +26,9 @@ class <%= class_name %> < <%= parent_class_name.classify %>
   # Callbacks
 
   # Associations
-<%- belongs_to_attrs = attributes_names.select{ |attr, value| attr.ends_with?('_id') } -%>
-<% belongs_to_attrs.each do |attr, value| -%>
-  belongs_to :<%= attr.gsub(/_id$/, '') %><%= ', polymorphic: true' if attributes_names.keys.include?(attr.gsub(/_id$/, '_type')) %>
+<% assoc_attrs.each do |attr, value| -%>
+<% next if attr.ends_with?('_type') -%>
+  belongs_to :<%= attr.gsub(/_id$/, '') %><%= ', polymorphic: true' if assoc_attrs.keys.include?(attr.gsub(/_id$/, '_type')) %>
 <% end -%>
 <% attributes.select(&:reference?).each do |attribute| -%>
   belongs_to :<%= attribute.name %><%= ', polymorphic: true' if attribute.polymorphic? %><%= ', required: true' if attribute.required? %>

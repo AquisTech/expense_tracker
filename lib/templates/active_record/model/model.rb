d5ab2941fc302ce58cc1c8d61204f712309644c9
AquisTech <anand.bait@gmail.com>
2020-11-15 09:30:32 +0530

    Update model scaffold to write polymorphic association

--- lib/templates/active_record/model/model.rb
+++ lib/templates/active_record/model/model.rb
@@ -22,8 +22,8 @@ class <%= class_name %> < <%= parent_class_name.classify %>
   # Associations
   <%- belongs_to_attrs = attributes_names.select{ |attr, value| k.ends_with?('_id') } -%>
   <% belongs_to_attrs.each do |attr, value| -%>
-belongs_to :<%= attr %>
-  <% end %>
+belongs_to :<%= attr %><%= ', polymorphic: true' if attribute_names.keys.include?(attr.gsub(/_id$/, '_type')) %>
+  <% end -%>
 <% attributes.select(&:reference?).each do |attribute| -%>
   belongs_to :<%= attribute.name %><%= ', polymorphic: true' if attribute.polymorphic? %><%= ', required: true' if attribute.required? %>
 <% end -%>
